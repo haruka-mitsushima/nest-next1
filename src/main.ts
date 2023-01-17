b7a0a9539f0fbfc6da097b82c76653b23191946b
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-17 11:39:22 +0900

    PrismaServiceの作成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, ValidationPipe } from '@nestjs/common';
+import cookieParser, * as CookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
